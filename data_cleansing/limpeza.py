038a83c49a2436fddd3bf6d17ae003afcd412a3c
Braias <bryansm00001@gmail.com>
2024-10-01 13:45:28 -0300

    adding cleansing module and zipcode data

--- data_cleansing/limpeza.py
+++ data_cleansing/limpeza.py
@@ -4,9 +4,9 @@ import geodatasets
 import matplotlib.pyplot as plt
 import pgeocode
 
-crashes = pd.read_csv("A1-LP\\.gitignore\\Motor_Vehicle_Collisions_-_Crashes.csv")
-#persons = pd.read_csv("A1-LP\\.gitignore\\Motor_Vehicle_Collisions_-_Person_20240925.csv")
-#vehicle = pd.read_csv("A1-LP\\.gitignore\\Motor_Vehicle_Collisions_-_Vehicles_20240925.csv")
+crashes = pd.read_csv("./dados/Motor_Vehicle_Collisions_-_Crashes.csv")
+#persons = pd.read_csv("./dados/Motor_Vehicle_Collisions_-_Person_20240925.csv")
+#vehicle = pd.read_csv("./dados/Motor_Vehicle_Collisions_-_Vehicles_20240925.csv")
 path_to_data = geodatasets.get_path("nybb")
 gdf = gpd.read_file(path_to_data)
 gdf.plot()
