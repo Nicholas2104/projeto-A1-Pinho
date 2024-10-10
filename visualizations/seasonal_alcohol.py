ae0873d1d9b5bcaac8deb33f9b1b4c11953f4160
sodrelds <sodressler2@gmail.com>
2024-10-10 17:13:55 -0300

    Translated the coments to english

--- visualizations/seasonal_alcohol.py
+++ visualizations/seasonal_alcohol.py
@@ -2,45 +2,45 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Ler o arquivo CSV
+# Reading csv file
 crashes = pd.read_csv("dados/Motor_Vehicle_Collisions_-_Crashes_20241008.csv")
 
-# Converter a coluna de datas para o formato datetime
+# Converting the column to the type datetime
 crashes["CRASH DATE"] = pd.to_datetime(crashes["CRASH DATE"], format="%m/%d/%Y")
 
-# Criar uma nova coluna para o mês (apenas o mês, sem o ano)
+# New column with the month name
 crashes["MONTH"] = crashes["CRASH DATE"].dt.month_name()
 
-# Filtrar os dados para acidentes causados por álcool e drogas
+# Filtering the accidents caused by alcohol and drugs
 drugs = ["Drugs (Illegal)", "Drugs (illegal)"]
 alcohol = ["Alcohol Involvement"]
 
 crashes_by_drugs = crashes[crashes["CONTRIBUTING FACTOR VEHICLE 1"].isin(drugs)]
 crashes_by_alcohol = crashes[crashes["CONTRIBUTING FACTOR VEHICLE 1"].isin(alcohol)]
 
-# Agrupar os dados por mês e contar o número de acidentes para cada categoria
+# Grouping data by month and couting the number of accidents for each category
 drug_accidents = crashes_by_drugs.groupby("MONTH").size().reset_index(name='Drug Accidents')
 alcohol_accidents = crashes_by_alcohol.groupby("MONTH").size().reset_index(name='Alcohol Accidents')
 
-# Mesclar os DataFrames
+# Merging the dataframes
 merged = drug_accidents.merge(alcohol_accidents, on="MONTH", how="outer")
-merged = merged.fillna(0)  # Substituir NaN por 0
+merged = merged.fillna(0)  # Replacing NaN values with 0
 
-# Ordenar os meses cronologicamente
+# Ordering the months
 month_order = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 merged["MONTH"] = pd.Categorical(merged["MONTH"], categories=month_order, ordered=True)
 merged = merged.sort_values(by="MONTH")
 
-# Reorganizar os dados para o gráfico de barras lado a lado
+# Reorganizing the data to use in the bar plot
 melted = pd.melt(merged, id_vars=["MONTH"], value_vars=["Drug Accidents", "Alcohol Accidents"], var_name="Type", value_name="Count")
 
-# Plotar o gráfico de barras lado a lado
+# Plotting the data
 f, ax = plt.subplots(figsize=(12, 6))
 
-# Plotar as barras lado a lado
+# Plotting the bar plot
 sns.barplot(x="MONTH", y="Count", hue="Type", data=melted, palette={"Drug Accidents": "r", "Alcohol Accidents": "g"})
 
-# Ajustar o gráfico
+# Adjust the plot
 ax.legend(ncol=1, loc="upper right", frameon=True)
 sns.despine(left=True, bottom=True)
 plt.xticks(rotation=45)
